743be32985aa5ab6ad840378a51349ecd4b9ba9d
Matthew C <matthewchuayy@gmail.com>
2025-06-24 13:41:33 +0800

    Update

--- eda-apr-25-cpi-core-headline.py
+++ eda-apr-25-cpi-core-headline.py
@@ -10,63 +10,65 @@ headline_inflation_data = r.get("https://api.data.gov.my/data-catalogue?id=cpi_h
 core_inflation_data = r.get("https://api.data.gov.my/data-catalogue?id=cpi_core_inflation").json()
 
 cinfdata = pd.DataFrame.from_records(core_inflation_data)
-cinfdata['date'] = pd.to_datetime(cinfdata['date']).dt.strftime('%Y-%m')
+cinfdata['date'] = pd.to_datetime(cinfdata['date'])
 cinfdata = cinfdata[cinfdata['division']== 'overall']
 cinfdata = cinfdata[['date', 'inflation_yoy']]
 
 # Manually imputing data, as the latest datapoint is not reflected in the API yet
-cinfdata_new = pd.DataFrame({'date': ['2025-04'], 'inflation_yoy': [2.0]})
-cinfdata = pd.concat([cinfdata, cinfdata_new], ignore_index=True)
+# cinfdata_new = pd.DataFrame({'date': ['2025-04'], 'inflation_yoy': [2.0]})
+# cinfdata = pd.concat([cinfdata, cinfdata_new], ignore_index=True)
 cinfdata = cinfdata.sort_values(by='date', ascending=False)
 
 # Renaming the column
-cinfdata = cinfdata.rename(columns={'inflation_yoy': 'Core CPI Year-on-Year Growth %'})
+cinfdata = cinfdata.rename(columns={'inflation_yoy': 'Core CPI YoY %'})
 
 infdata = pd.DataFrame.from_records(headline_inflation_data)
-infdata['date'] = pd.to_datetime(infdata['date']).dt.strftime('%Y-%m')
+infdata['date'] = pd.to_datetime(infdata['date'])
 infdata = infdata[infdata['division']== 'overall']
 infdata = infdata[['date', 'inflation_yoy']]
 
-# Manually imputing data, as the latest datapoint is not reflected in the API yet
-infdata_new = pd.DataFrame({'date': ['2025-04'], 'inflation_yoy': [1.4]})
-infdata = pd.concat([infdata, infdata_new], ignore_index=True)
-infdata = infdata.sort_values(by='date', ascending=False)
+# # Manually imputing data, as the latest datapoint is not reflected in the API yet
+# infdata_new = pd.DataFrame({'date': ['2025-04'], 'inflation_yoy': [1.4]})
+# infdata = pd.concat([infdata, infdata_new], ignore_index=True)
+# infdata = infdata.sort_values(by='date', ascending=False)
 
 # Renaming the column
-infdata = infdata.rename(columns={'inflation_yoy': 'Headline CPI Year-on-Year Growth %'})
+infdata = infdata.rename(columns={'inflation_yoy': 'Headline CPI YoY %'})
 
 # Merging the DataFrames
 inflation = pd.merge(infdata, cinfdata, on='date')
 inflation = inflation.sort_values(by='date', ascending=False)
 
 # Filter for datapoints across the past 12 months starting from May 2024
-inflation = inflation[inflation['date']>'2024-04']
+inflation = inflation[inflation['date']>'2024-05-01']
 inflation = inflation.sort_values(by='date', ascending=True)
 
-# Create a Combo Chart
+fig, ax1 = plt.subplots(figsize=(6, 6))
 
-fig, ax1 = plt.subplots(figsize=(12, 6))
-
-# Create the bar chart on the first axis (left y-axis)
-ax1.plot(inflation['date'], inflation['Headline CPI Year-on-Year Growth %'], color='grey', marker='o', linestyle='-', label='Headline CPI Year-on-Year Growth %')
-ax1.set_ylabel('Headline CPI Year-on-Year Growth %', color='black')
-ax1.tick_params(axis='y', labelcolor='black')
-ax1.set_xlabel('Date', color='black')
-
-# Create the second axis (right y-axis)
+# Plot the two lines
+ax1.plot(inflation['date'], inflation['Headline CPI YoY %'], color='red', marker='o', label='Headline CPI (YoY) %')
 ax2 = ax1.twinx()
+ax2.plot(inflation['date'], inflation['Core CPI YoY %'], color='black', marker='o', linestyle = '--', label='Core CPI (YoY) %')
 
-# Create the line chart on the second axis (right y-axis)
-ax2.plot(inflation['date'], inflation['Core CPI Year-on-Year Growth %'], color='black', marker='o', linestyle='-', label='Core CPI Year-on-Year Growth %')
-ax2.set_ylabel('Core CPI Year-on-Year Growth %', color='black')
-ax2.tick_params(axis='y', labelcolor='black')
+# Set y-label
+ax1.set_ylabel('YoY Changes %', color='#5a5a5a')
+
+# Set title
+ax1.set_title('Headline vs Core CPI (YoY) (As at May 2025)', fontsize=20, color='#5a5a5a')
 
 # Combine legends from both axes
 lines, labels = ax1.get_legend_handles_labels()
 lines2, labels2 = ax2.get_legend_handles_labels()
 ax2.legend(lines + lines2, labels + labels2, loc='lower left')
 
-plt.title('Headline vs Core CPI Year-on-Year Growth (As at April 2025)', color='black')
-plt.xticks(rotation=45)
-plt.tight_layout()
+# Set chart box color
+for ax in [ax1, ax2]:
+    for spine in ax.spines.values():
+        spine.set_edgecolor('#cecece')
+
+# No x-axis title
+ax1.set_xlabel('')
+
+# Set grid lines
+ax1.yaxis.grid(color='grey', linestyle='--', linewidth=0.5, alpha=0.7)
 plt.show()
--- eda-mar-25-my-economic-indicators.py
+++ eda-mar-25-my-economic-indicators.py
@@ -1,30 +1,16 @@
-# %%
 import pandas as pd
 import requests as r
 from datetime import datetime
 import matplotlib.pyplot as plt
-import seaborn as sns
 
-# %%
 # Get the datasource
 mei_data = r.get("https://api.data.gov.my/data-catalogue?id=economic_indicators").json()
 
 meidata = pd.DataFrame.from_records(mei_data)
-meidata['date'] = pd.to_datetime(meidata['date']).dt.strftime('%Y-%m')
+meidata['date'] = pd.to_datetime(meidata['date'])
 
-
-# %%
-# Manually imputing data, as the latest datapoint is not reflected in the API yet
-meidata_new = pd.DataFrame({'date': ['2025-03'], 
-                            'lagging': [145.4],
-                            'leading': [112.5],
-                            'coincident': [126.8],
-                            'leading_diffusion': [71.400000],
-                            'coincident_diffusion': [66.700000],})
-meidata = pd.concat([meidata, meidata_new], ignore_index=True)
 meidata = meidata.sort_values(by='date', ascending=False)
 
-# %%
 # Calculate YoY changes for lagging, leading, and coincident indexes
 meidata_yoy = meidata.copy()
 for idx in ['lagging', 'leading', 'coincident']:
@@ -35,34 +21,31 @@ cols_to_keep = ['date', 'leading_yoy', 'coincident_yoy']
 meidata_yoy = meidata_yoy[cols_to_keep]
 meidata_yoy = meidata_yoy.sort_values('date', ascending=True).reset_index(drop=True)
 
-# %%
-meidata_yoy_12m = meidata_yoy[meidata_yoy['date']>'2024-02']
+meidata_yoy_12m = meidata_yoy[meidata_yoy['date']>'2024-03-01']
 meidata_yoy_12m = meidata_yoy_12m.sort_values(by='date', ascending=True).reset_index(drop=True)
 meidata_cols_yoy_12m = meidata_yoy_12m.columns.tolist()
 # Convert the data to 1 decimal place, except for the date
 for col in meidata_cols_yoy_12m[1:]:
     meidata_yoy_12m[col] = meidata_yoy_12m[col].round(1)
 
-# %%
-meidata_12m = meidata[meidata['date']>'2024-02']
+meidata_12m = meidata[meidata['date']>'2024-03-01']
 meidata_12m = meidata_12m.sort_values(by='date', ascending=True).reset_index(drop=True)
 meidata_cols_12m = meidata_12m.columns.tolist()
 # Convert the data to 1 decimal place, except for the date
 for col in meidata_cols_12m[1:]:
     meidata_12m[col] = meidata_12m[col].round(1)
 
-# %%
-fig, ax = plt.subplots(figsize=(16, 9))
+fig, ax = plt.subplots(figsize=(6, 6))
 
-# Plot the two lines in greyscale
+# Plot the two lines
 ax.plot(meidata_yoy_12m['date'], meidata_yoy_12m['leading_yoy'], color='red', marker='o', label='Leading YoY')
-ax.plot(meidata_yoy_12m['date'], meidata_yoy_12m['coincident_yoy'], color='#868585', marker='o', linestyle = '--', label='Coincident YoY')
+ax.plot(meidata_yoy_12m['date'], meidata_yoy_12m['coincident_yoy'], color='black', marker='o', linestyle = '--', label='Coincident YoY')
 
 # Set y-label
 ax.set_ylabel('YoY Changes %', color='#5a5a5a')
 
 # Set title
-ax.set_title('Malaysian Economic Indicators March 2025', fontsize=20, color='#5a5a5a')
+ax.set_title('Malaysian Economic Indicators April 2025', fontsize=20, color='#5a5a5a')
 
 # Set legend at bottom left
 ax.legend(loc='lower left', frameon=True)
@@ -74,57 +57,6 @@ for spine in ax.spines.values():
 # No x-axis title
 ax.set_xlabel('')
 
-# Improve x-tick labels
-ax.set_xticklabels(meidata_yoy_12m['date'], rotation=45, ha='right', color="#5a5a5a")
-ax.tick_params(axis='x', colors='#5a5a5a')
-ax.tick_params(axis='y', colors='#5a5a5a')
-
 # Set grid lines
 ax.yaxis.grid(color='grey', linestyle='--', linewidth=0.5, alpha=0.7)
 plt.show()
-
-# %%
-fig, ax1 = plt.subplots(figsize=(16, 9))
-
-# Bar chart for YoY changes (primary y-axis)
-bar_width = 0.25
-x = range(len(meidata_yoy_12m['date']))
-
-ax1.bar(x, meidata_yoy_12m['leading_yoy'], width=bar_width, color="#868585", label='Leading YoY')
-ax1.bar([i + bar_width for i in x], meidata_yoy_12m['coincident_yoy'], width=bar_width, color="#5F5E5E", label='Coincident YoY')
-
-ax1.set_ylabel('YoY Changes %', color='#5a5a5a')
-ax1.tick_params(axis='y', colors='#5a5a5a')
-ax1.set_xticks(x)
-ax1.set_xticklabels(meidata_yoy_12m['date'], rotation=45, ha='right', color="#5a5a5a")
-ax1.tick_params(axis='x', colors='#5a5a5a')
-
-# # Secondary y-axis for diffusion index
-# ax2 = ax1.twinx()
-# ax2.set_ylabel('Diffusion Index %', color='#5a5a5a')
-# ax2.plot(x, meidata_12m['leading_diffusion'], color="#4E4E4E", linestyle='--', linewidth=1, marker='.', label='Leading Diffusion')
-# ax2.plot(x, meidata_12m['coincident_diffusion'], color="#000000", linestyle='--', linewidth=1, marker='.', label='Coincident Diffusion')
-
-# Remove chart box
-for spine in ax1.spines.values():
-    spine.set_visible(False)
-# for spine in ax2.spines.values():
-#     spine.set_visible(False)
-
-ax1.axvline(x=-1, color='#cecece', linewidth=1, linestyle='-')
-# ax2.axvline(x=13, color='#cecece', linewidth=1, linestyle='-')
-ax1.axhline(y=0, color='#cecece', linewidth=1, linestyle='-')
-
-# Legends
-bars_labels = ['Leading YoY', 'Coincident YoY']
-# lines_labels = ['Leading Diffusion', 'Coincident Diffusion']
-bars = [plt.Rectangle((0,0),1,1, color=c) for c in ['#868585', '#5F5E5E']]
-# lines = [plt.Line2D([0], [0], color=c, linestyle='-', marker='o') for c in ["#4E4E4E", "#000000"]]
-# ax1.legend(bars + lines, bars_labels + lines_labels, loc='upper right', frameon=True)
-ax1.legend(bars, bars_labels, loc='upper right', frameon=True)
-
-plt.title('Malaysian Economic Indicators March 2025', fontsize=20, color='#5a5a5a')
-# plt.grid(True, linestyle='--', linewidth=0.5, color='#cecece')
-plt.show()
-
-
